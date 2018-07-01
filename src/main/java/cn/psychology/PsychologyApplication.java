c6adbfff8310c0a38859e343284df4bb5bd3338f
John-Snow <hmqq150@163.com>
2018-07-01 13:32:10 +0800

    ceshi11

--- src/main/java/cn/psychology/PsychologyApplication.java
+++ src/main/java/cn/psychology/PsychologyApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@ServletComponentScan
 public class PsychologyApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
